0a9c739316d1c5539f3e165494a8af0f4ce479f2
AppDevIn <teamprojectlive@gmail.com>
2021-01-16 23:01:08 +0800

    🎉 Init Project

---
+++ TravelLog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelLog
+//
+//  Created by Jeya Vishnu on 16/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
