df41c9213cb980030feb690c98d16350cda1f8af
AppDevIn <teamprojectlive@gmail.com>
2021-01-20 16:01:54 +0800

    Retrieve link the text and buttons. adding smart dashes

--- TravelLog/Controller/LoginController.swift
+++ TravelLog/Controller/LoginController.swift
@@ -11,8 +11,23 @@ import UIKit
 
 class LoginController : UIViewController {
     
+    @IBOutlet weak var txt_email: UITextField!
+    @IBOutlet weak var txt_password: UITextField!
+    
     override func viewDidLoad() {
         self.navigationController?.isNavigationBarHidden = true
+        txt_password.isSecureTextEntry = true
+    }
+    
+    @IBAction func loginClicked(_ sender: Any) {
+        guard let email:String =  txt_email.text else { return }
+        guard let password:String = txt_password.text else { return }
+        
+        print("Email " + email)
+        print("Password " + password)
+    }
+    
+    @IBAction func signUpClicked(_ sender: Any) {
     }
     
 }
