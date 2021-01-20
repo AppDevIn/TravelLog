e2102adc1bb6fc8efaab2dc12a37cab64b057b02
AppDevIn <teamprojectlive@gmail.com>
2021-01-20 15:01:27 +0800

    🌱 Google service file

---
+++ TravelLog/Controller/LoginController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginController.swift
+//  TravelLog
+//
+//  Created by Jeya Vishnu on 20/1/21.
+//
+
+import Foundation
+import UIKit
+
+
+class LoginController : UIViewController {
+    
+    override func viewDidLoad() {
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+}
+
