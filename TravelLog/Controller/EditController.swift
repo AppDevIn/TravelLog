d7f35c375579bb00a0642edc16787fad88438531
AppDevIn <teamprojectlive@gmail.com>
2021-01-25 02:40:30 +0800

    🌱 Edit controller file

---
+++ TravelLog/Controller/EditController.swift
@@ -0,0 +1,15 @@
+//
+//  EditController.swift
+//  TravelLog
+//
+//  Created by Jeya Vishnu on 25/1/21.
+//
+
+import Foundation
+import UIKit
+
+class EditController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
