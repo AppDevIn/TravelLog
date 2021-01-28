e425dcc29009599edf2e476e9fc09746948acbe8
AppDevIn <teamprojectlive@gmail.com>
2021-01-29 00:20:08 +0800

    🌱 Init Search Controller

---
+++ TravelLog/Controller/SearchController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchController.swift
+//  TravelLog
+//
+//  Created by Jeya Vishnu on 29/1/21.
+//
+
+import Foundation
+import UIKit
+
+
+class SearchController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
