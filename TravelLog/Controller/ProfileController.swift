786e6128dddc95ec382822cb02baf4cb1af5f284
AppDevIn <teamprojectlive@gmail.com>
2021-01-28 09:45:26 +0800

    Setup delegate and datasoure

--- TravelLog/Controller/ProfileController.swift
+++ TravelLog/Controller/ProfileController.swift
@@ -10,10 +10,43 @@ import UIKit
 
 
 class ProfileController:UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
     }
     
     
 }
+
+
+extension ProfileController:UICollectionViewDelegate{
+    
+    //When a cell is selected
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+    }
+}
+
+extension ProfileController:UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "", for: indexPath)
+        
+        return cell
+    }
+}
+
+//Can specify the padding and width
+extension ProfileController:UICollectionViewDelegateFlowLayout{}
+
+
+
