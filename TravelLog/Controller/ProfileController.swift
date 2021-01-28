42abda4c12c38803d53b42a67968c367f8949845
AppDevIn <teamprojectlive@gmail.com>
2021-01-28 09:39:14 +0800

    🌱  Init Profile view controller

---
+++ TravelLog/Controller/ProfileController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileController.swift
+//  TravelLog
+//
+//  Created by Jeya Vishnu on 28/1/21.
+//
+
+import Foundation
+import UIKit
+
+
+class ProfileController:UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
