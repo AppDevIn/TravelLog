1a0a9e432b1cfe5fae6fc58b4f15a2ebc1546941
AppDevIn <teamprojectlive@gmail.com>
2021-02-01 00:15:03 +0800

    🚧 Show firebase error message

--- TravelLog/Controller/SplashScreenController.swift
+++ TravelLog/Controller/SplashScreenController.swift
@@ -30,7 +30,9 @@ class SplashScreenController : UIViewController {
                 //Check if there is error
                 guard let user = authResult?.user, error == nil else {
                     print(error!.localizedDescription)
-                    self.dismiss(animated: true, completion: nil)
+                    self.alert(title: "Error", message: error!.localizedDescription)
+                    
+                    
                     return
                 }
                 
@@ -63,8 +65,7 @@ class SplashScreenController : UIViewController {
                 //Check if there is error
                 guard let user = authResult?.user, error == nil else {
                     print(error!.localizedDescription)
-                    self.dismiss(animated: true, completion: nil)
-                    
+                    self.alert(title: "Error", message: error!.localizedDescription)
                     return
                 }
                 
@@ -106,6 +107,7 @@ class SplashScreenController : UIViewController {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
         NSLog("The \"OK\" alert occured.")
+            self.dismiss(animated: true, completion: nil)
         }))
         
         
