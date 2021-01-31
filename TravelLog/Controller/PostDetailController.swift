ffc3629f61ce98ff74ae9c77b90b261f5e14f46c
AppDevIn <teamprojectlive@gmail.com>
2021-02-01 01:07:33 +0800

    ✨ Swipe through images

--- TravelLog/Controller/PostDetailController.swift
+++ TravelLog/Controller/PostDetailController.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import SDWebImage
 
 class PostDetailController: UIViewController {
     
@@ -16,6 +17,9 @@ class PostDetailController: UIViewController {
     @IBOutlet weak var postBody: UITextView!
     var feed:HomeFeed?
     var post: Post!
+    
+    
+    var currentImage:Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,7 +36,66 @@ class PostDetailController: UIViewController {
             self.postLocation.text = post.locations
             self.postBody.text = post.decription
         }
+        
+        self.postImg.isUserInteractionEnabled = true
+        
+        //Setting up the gestue for right
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(self.respondToSwipeGesture))
+        swipeRight.direction = .right
+        self.postImg.addGestureRecognizer(swipeRight)
+        
+        //Setting up the gestue for left
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(self.respondToSwipeGesture))
+        swipeLeft.direction = .left
+        self.postImg.addGestureRecognizer(swipeLeft)
     }
+    
+    @objc func respondToSwipeGesture(gesture: UIGestureRecognizer?) -> Void {
+        
+        print("Swipped")
+
+        if (feed?.postImages.count)! <= 0 {
+            print("No images")
+            return
+        }
+
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+
+
+            //Check which directiuon is being swipped
+            switch swipeGesture.direction {
+            //Swipped Left
+            case UISwipeGestureRecognizer.Direction.left:
+
+                // Add the current Image interger to go front
+                if currentImage == (feed?.postImages.count)! - 1 { currentImage = 0 }
+                else { currentImage += 1 }
+
+                displayImages(i: currentImage)
+
+            //Swipped Right
+            case UISwipeGestureRecognizer.Direction.right:
+
+                // Minus the current Image interger to go back
+                if currentImage == 0 { currentImage = (feed?.postImages.count)! - 1 }
+                else { currentImage -= 1 }
+
+                displayImages(i: currentImage)
+            default:
+                break
+            }
+        }
+    }
+    
+    
+    func displayImages(i index:Int){
+        
+        //Cover string to URL
+        let url:URL = NSURL(string: feed?.postImages[index] as! String )! as URL
+        self.postImg.sd_setImage(with: url, placeholderImage: UIImage(named: "FooterLogin"))
+        
+    }
+    
     
     
     
